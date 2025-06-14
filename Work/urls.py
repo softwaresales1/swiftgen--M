a39d1cc1ca8b9a5d9b1f392355b70b3946cd93f2
SWIFT TALENT FORGE MARKETPLACE 2.0 <waterdrop198@gmail.com>
2025-06-14 20:49:32 +0300

    Update urls.py

--- Work/urls.py
+++ Work/urls.py
@@ -24,3 +24,8 @@ urlpatterns = [
      path('chat/', include('chat.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Add this for serving media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
